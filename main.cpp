a7cc6de98e464d3c0930a844e3999a61d2cede15
Piromancer <44932328+Piromancer@users.noreply.github.com>
2020-06-22 16:50:26 +0300

    New main cases

--- main.cpp
+++ main.cpp
@@ -68,79 +68,23 @@ TEST_F(Smoke, rect_test) {
 	EXPECT_EQ(rect.Height(), -rect.Width());
 	EXPECT_EQ(rect.Height(), -1);
 }
-TEST_F(Smoke, kernel_compute_complex) {
-	g_AMFFactory.GetFactory()->SetCacheFolder(L"./cache");
 
-	amf::AMFPrograms* pPrograms;
-	factory->GetPrograms(&pPrograms);
+TEST_F(Smoke, get_compute) {
+	AMFCompute* compute;
+	context1->GetCompute(AMF_MEMORY_OPENCL, &compute);
+	EXPECT_TRUE(compute);
+}
 
-	amf::AMF_KERNEL_ID kernel = 0;
-	const char* kernel_src = "\n" \
-		"__kernel void square2( __global float* input, __global float* output, \n" \
-		" const unsigned int count) {            \n" \
-		" int i = get_global_id(0);              \n" \
-		" if(i < count) \n" \
-		" output[i] = input[i] * input[i]; \n" \
-		"}                     \n";
-	pPrograms->RegisterKernelSource(&kernel, L"kernelIDName", "square2", strlen(kernel_src), (amf_uint8*)kernel_src, NULL);
+TEST_F(Smoke, get_device_at) {
+	AMFComputeDevice* device;
+	oclComputeFactory->GetDeviceAt(0, &device);
+	amf::AMFComputePtr pCompute;
+	device->CreateCompute(nullptr, &pCompute);
+	EXPECT_TRUE(device);
+	EXPECT_TRUE(pCompute);
+}
 
-	for (int i = 0; i < deviceCount; ++i)
-	{
-		AMF_RESULT res;
-		amf::AMFComputeDevicePtr pComputeDevice;
-		oclComputeFactory->GetDeviceAt(i, &pComputeDevice);
-		pComputeDevice->GetNativeContext();
-
-		amf::AMFComputePtr pCompute;
-		pComputeDevice->CreateCompute(nullptr, &pCompute);
-
-		amf::AMFComputeKernelPtr pKernel;
-		res = pCompute->GetKernel(kernel, &pKernel);
-
-		amf::AMFBuffer* input = NULL;
-		amf::AMFBuffer* output = NULL;
-
-		amf::AMFContextPtr context;
-		factory->CreateContext(&context);
-		//context->InitOpenCLEx(pComputeDevice.GetPtr());
-		context->InitOpenCL(pCompute->GetNativeCommandQueue());
-
-		res = context->AllocBuffer(amf::AMF_MEMORY_HOST, 1024 * sizeof(float), &input);
-		res = context->AllocBuffer(amf::AMF_MEMORY_OPENCL, 1024 * sizeof(float), &output);
-
-		float* inputData = static_cast<float*>(input->GetNative());
-		float* expectedData = new float[1024];
-		for (int k = 0; k < 1024; k++)
-		{
-			inputData[k] = rand() / 50.00;
-			expectedData[k] = inputData[k] * inputData[k];
-		}
-
-		input->Convert(amf::AMF_MEMORY_OPENCL);
-
-		res = pKernel->SetArgBuffer(1, output, amf::AMF_ARGUMENT_ACCESS_WRITE);
-		res = pKernel->SetArgBuffer(0, input, amf::AMF_ARGUMENT_ACCESS_READ);
-		res = pKernel->SetArgInt32(2, 1024);
-
-		amf_size sizeLocal[3] = { 1024, 0, 0 };
-		amf_size sizeGlobal[3] = { 1024, 0, 0 };
-		amf_size offset[3] = { 0, 0, 0 };
-
-		pKernel->GetCompileWorkgroupSize(sizeLocal);
-
-		pKernel->Enqueue(1, offset, sizeGlobal, sizeLocal);
-		pCompute->FlushQueue();
-		pCompute->FinishQueue();
-		float* outputData2 = NULL;
-		res = output->MapToHost((void**)&outputData2, 0, 1024 * sizeof(float), true);
-
-
-		for (int k = 0; k < 1024; k++)
-		{
-			EXPECT_LE(abs(expectedData[k] - outputData2[k]), 0.01);
-		}
-
-		output->Convert(amf::AMF_MEMORY_HOST);
-		float* outputData = static_cast<float*>(output->GetNative());
-	}
+TEST_F(Smoke, get_device_at_negative) {
+	AMFComputeDevice* device;
+	EXPECT_ANY_THROW(oclComputeFactory->GetDeviceAt(1000, &device));
 }
